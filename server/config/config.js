5b919dc302a2ff496a8496b80d83cb9c02292095
Jean <jeancwp@hotmail.com>
2019-10-30 23:00:02 -0500

    Variable de Entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/mathbook';
 } else {
-    urlDB = 'mongodb+srv://jeancwp:ZQkRQCkb1fTVxHf2@cluster0-qe8wd.mongodb.net/mathbook?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
