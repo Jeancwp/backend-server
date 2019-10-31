22064e1fb8cb0841406de6d5b81c55cf3d318a37
Jean <jeancwp@hotmail.com>
2019-10-30 22:44:53 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 // Puerto
 // =======================
 process.env.PORT = process.env.PORT || 3000;
+
+// =======================
+// Entorno
+// =======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======================
+// Base de Datos
+// =======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/mathbook';
+} else {
+    urlDB = 'mongodb+srv://jeancwp:ZQkRQCkb1fTVxHf2@cluster0-qe8wd.mongodb.net/mathbook?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
